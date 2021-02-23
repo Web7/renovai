f1bc1db1b74609f816a483b47c60cc3169fa7f45
Oleksandr Nikitin <a.nikitin@i.ua>
2021-02-23 13:52:08 +0200

    sections

--- src/js/common.js
+++ src/js/common.js
@@ -13,9 +13,40 @@
 		return this.length !== 0;
 	};
 
+	var counterSectionRcsarTop;
+	var windowHeight;
+	var show = false;
 
 	$(function () {
+		var $counterSectionRcsar = $('.counter-section-rcsar');
 
+
+		if ($counterSectionRcsar.exists()) {
+			counterSectionRcsarTop = $counterSectionRcsar.offset().top;
+			windowHeight = window.innerHeight;
+			show = true;
+		}
+	});
+
+	$(window).on('scroll', function(){
+		if(show && (counterSectionRcsarTop < $(window).scrollTop() + windowHeight)){
+			var $dataCounter = $('[data-counter]');
+			if ($dataCounter.exists()) {
+				$dataCounter.each(function(){
+					var $this = $(this);
+					$this.prop('counter', 0).animate({
+						counter: $this.attr('data-counter')
+					}, {
+						duration: 1000,
+						easing: 'swing',
+						step: function(now) {
+							$this.text(this.counter.toFixed(0) + '%')
+						}
+					})
+				});
+			}
+			show = false;
+		}
 	});
 
 }, window.jQuery, window.Zepto));
