0ced5aefe483df544f045b7a01dc6876dd43c868
Oleksandr Nikitin <a.nikitin@i.ua>
2024-10-24 23:08:30 +0300

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,30 @@
+const fs = require('fs');
 const path = require('path');
-const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const PAGES = fs.readdirSync('./src/components/pages/').filter(fileName => fileName.endsWith('.pug'));
 
 const configureCopy = () => {
 	return [
-		{from: "src/video/", to: "video/"},
-		{from: "src/images/", to: "images/"},
-		{from: 'src/fonts/', to: 'fonts/'}
+		// {from: "src/video/", to: "video/"},
+		{from: 'src/images/', to: 'images/'},
+		// {from: 'src/fonts/', to: 'fonts/'},
+		{from: 'src/js/', to: 'js/'},
+		{from: path.resolve('node_modules/jquery/dist/jquery.min.js'), to: path.resolve('docs/js/')},
+		{from: path.resolve('node_modules/popper.js/dist/popper.min.js'), to: path.resolve('docs/js/')},
+		{from: path.resolve('node_modules/bootstrap/dist/js/bootstrap.min.js'), to: path.resolve('docs/js/')},
+		{from: path.resolve('node_modules/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js'), to: path.resolve('docs/js/')},
+		{from: path.resolve('node_modules/bootstrap-datepicker/js/locales/bootstrap-datepicker.uk.js'), to: path.resolve('docs/js/')},
+		{from: path.resolve('node_modules/perfect-scrollbar/dist/perfect-scrollbar.min.js'), to: path.resolve('docs/js/')},
+		{from: path.resolve('node_modules/dotdotdot-js/dist/dotdotdot.js'), to: path.resolve('docs/js/')},
+		{from: path.resolve('node_modules/slick-carousel/slick/slick.min.js'), to: path.resolve('docs/js/')},
+		{from: path.resolve('node_modules/intl-tel-input/build/js/intlTelInput-jquery.min.js'), to: path.resolve('docs/js/')},
+		{from: path.resolve('src/js/button-scroll-to-top.js'), to: path.resolve('docs/js/')},
+		{from: path.resolve('src/js/scroll-toggle-menu.js'), to: path.resolve('docs/js/')},
+		{from: path.resolve('src/js/select-js.js'), to: path.resolve('docs/js/')},
+		{from: path.resolve('src/js/common.js'), to: path.resolve('docs/js/')},
 	]
 };
 
@@ -21,14 +36,8 @@ module.exports = {
 	},
 
 	output: {
-		filename: 'js/index.js',
-		path: path.resolve(__dirname, 'dist')
+		path: path.resolve(__dirname, 'docs')
 	},
-
-	// plugins: [
-	// 	new webpack.ProgressPlugin(),
-	// 	new MiniCssExtractPlugin({filename: 'common.[contenthash].css'})
-	// ],
 
 	module: {
 		rules: [
@@ -41,38 +50,39 @@ module.exports = {
 			},
 			{
 				test: /\.(svg|png|jpg|gif|jpeg)$/,
-				use: {
-					loader: 'file-loader',
-					options: {
-						name: 'images/[name].[ext]',
-						outputPath: 'images/',
-						publicPath: '../'
-					}
-				}
+				exclude: /src\/fonts/,
+				// use: [{
+				//     loader: 'file-loader',
+				//     options: {
+				//         name: '[name].[ext]',
+				//         outputPath: 'images/'
+				//     }
+				// }]
+				generator: {
+					filename: 'images/[name][ext]'
+				},
 			},
 			{
 				test: /\.(mp4)$/,
-				use: {
-					loader: 'file-loader',
-					options: {
-						name: 'video/[name].[ext]',
-						outputPath: 'video/',
-						publicPath: '../'
-					}
-				}
+				type: 'asset/resource',
+				generator: {
+					filename: 'video/[name][ext]'
+				},
 			},
 			{
-				test: /\.(woff|woff2|eot|ttf|svg)$/,
-				use: [
-					{
-						loader: 'file-loader',
-						options: {
-							name: 'fonts/[name].[ext]',
-							outputPath: 'fonts/',
-							publicPath: '../'
-						}
-					}
-				]
+				test: /\.(woff(2)?|ttf|eot|svg|otf)(\?v=\d+\.\d+\.\d+)?$/,
+				exclude: /src\/images/,
+				// use: [{
+				//     loader: 'file-loader',
+				//     options: {
+				//         name: '[name].[ext]',
+				//         outputPath: 'fonts/'
+				//     }
+				// }]
+				// type: 'asset/resource',
+				generator: {
+					filename: 'fonts/[name][ext]'
+				}
 			},
 			{
 				test: /\.pug$/,
@@ -96,128 +106,27 @@ module.exports = {
 		]
 	},
 
+	devServer: {
+		port: 8080,
+		hot: true,
+		open: true,
+	},
+
 	plugins: [
 		new CopyWebpackPlugin({
 			patterns: configureCopy()
 		}),
-		new HtmlWebPackPlugin({
-			template: './src/components/pages/index.pug',
-			filename: 'index.html',
-			file: require('./src/data/white-menu.json'),
-			cache: false
-		}),
-		new HtmlWebPackPlugin({
-			template: './src/components/pages/product-page.pug',
-			filename: 'product-page.html',
-			file: require('./src/data/black-menu.json'),
-			cache: false
-		}),
-		new HtmlWebPackPlugin({
-			template: './src/components/pages/careers.pug',
-			filename: 'careers.html',
-			file: require('./src/data/black-menu.json'),
-			cache: false
-		}),
-		new HtmlWebPackPlugin({
-			template: './src/components/pages/job-application.pug',
-			filename: 'job-application.html',
-			file: require('./src/data/black-menu.json'),
-			cache: false
-		}),
-		new HtmlWebPackPlugin({
-			template: './src/components/pages/case-studies.pug',
-			filename: 'case-studies.html',
-			file: require('./src/data/white-menu.json'),
-			cache: false
-		}),
-		new HtmlWebPackPlugin({
-			template: './src/components/pages/case-studies-item.pug',
-			filename: 'case-studies-item.html',
-			file: require('./src/data/white-menu.json'),
-			cache: false
-		}),
-		new HtmlWebPackPlugin({
-			template: './src/components/pages/case-studies-got-it.pug',
-			filename: 'case-studies-got-it.html',
-			file: require('./src/data/white-menu.json'),
-			cache: false
-		}),
-		new HtmlWebPackPlugin({
-			template: './src/components/pages/blog.pug',
-			filename: 'blog.html',
-			file: require('./src/data/white-menu.json'),
-			cache: false
-		}),
-		new HtmlWebPackPlugin({
-			template: './src/components/pages/blog-item.pug',
-			filename: 'blog-item.html',
-			file: require('./src/data/white-menu.json'),
-			cache: false
-		}),
-		new HtmlWebPackPlugin({
-			template: './src/components/pages/news.pug',
-			filename: 'news.html',
-			file: require('./src/data/white-menu.json'),
-			cache: false
-		}),
-		new HtmlWebPackPlugin({
-			template: './src/components/pages/page-not-found.pug',
-			filename: 'page-not-found.html',
-			file: require('./src/data/white-menu.json'),
-			cache: false
-		}),
-		new HtmlWebPackPlugin({
-			template: './src/components/pages/book-a-demo.pug',
-			filename: 'book-a-demo.html',
-			file: require('./src/data/black-menu.json'),
-			cache: false
-		}),
-		new HtmlWebPackPlugin({
-			template: './src/components/pages/thank-you.pug',
-			filename: 'thank-you.html',
-			file: require('./src/data/black-menu.json'),
-			cache: false
-		}),
-		new HtmlWebPackPlugin({
-			template: './src/components/pages/contact-thank-you.pug',
-			filename: 'contact-thank-you.html',
-			file: require('./src/data/white-menu.json'),
-			cache: false
-		}),
-		new HtmlWebPackPlugin({
-			template: './src/components/pages/reports-white-papers.pug',
-			filename: 'reports-white-papers.html',
-			file: require('./src/data/white-menu.json'),
-			cache: false
-		}),
-		new HtmlWebPackPlugin({
-			template: './src/components/pages/about.pug',
-			filename: 'about.html',
-			file: require('./src/data/black-menu.json'),
-			cache: false
-		}),
-		new HtmlWebPackPlugin({
-			template: './src/components/pages/contact-us.pug',
-			filename: 'contact-us.html',
-			file: require('./src/data/white-menu.json'),
-			cache: false
-		}),
-		new HtmlWebPackPlugin({
-			template: './src/components/pages/general-terms-and-conditions.pug',
-			filename: 'general-terms-and-conditions.html',
-			file: require('./src/data/white-menu.json'),
-			cache: false
-		}),
-		new HtmlWebPackPlugin({
-			template: './src/components/pages/studio-lp.pug',
-			filename: 'studio-lp.html',
-			file: require('./src/data/white-menu.json'),
-			cache: false
-		}),
 		new MiniCssExtractPlugin({
-			filename: 'css/style.css',
+			filename: 'css/stylesheet.css',
 			chunkFilename: '[id].css'
-		})
+		}),
+		...PAGES.map(page => new HtmlWebPackPlugin({
+				template: `./src/components/pages/${page}`,
+				filename: `${page.replace(/\.pug/, '.html')}`,
+				file: require('./src/data/index.json'),
+				cache: false
+			})
+		)
 	],
 
 	optimization: {
